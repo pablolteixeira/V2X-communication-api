f0dab72af49909707d26e54716635efcfc5b4043
pablolteixeira <xpspablo@hotmail.com>
2025-04-11 15:06:59 -0300

    fix: fixed the comparer

--- header/ordered_list.h
+++ header/ordered_list.h
@@ -1,46 +1,49 @@
 #ifndef ORDERED_LIST_H
 #define ORDERED_LIST_H
 
+#include <iostream>
 #include <list>
+#include <iterator>
 
 template<typename T, typename C = void>
 class Ordered_List 
 {
 public:
-    class Iterator 
-    {
-    public:
-        Iterator(typename std::list<T*>::iterator it) : _it(it) {}
-        Iterator& operator++() { ++_it; return *this; }
-        bool operator!=(const Iterator& other) const { return _it != other._it; }
-        T* operator->() const { return *_it; }
-        T* operator*() const { return *_it; }
-
-    private:
-        typename std::list<T*>::iterator _it;
-    };
-
     Ordered_List() {}
     ~Ordered_List() {}
 
     void insert(T* item) {
+        if (!item) return;
+
         auto it = _items.begin();
-        while (it != _items.end() && comp(**it, *item)) {
+
+        while (it != _items.end() && **it < *item) {
+            if (**it == *item) { 
+                return;
+            }
+
             ++it;
         }
-        _items.insert(it, item);
+
+        _items.insert(it, item);        
     }
 
     void remove(T* item) {
+        if (!item) return;
         _items.remove(item);
     }
 
-    Iterator begin() { return Iterator(_items.begin()); }
-    Iterator end() { return Iterator(_items.end()); }
+    void print() const {
+        std::cout << "[";
+        for (auto it = _items.begin(); it != _items.end(); it++) {
+            std::cout << **it << " ";
+        }
+    
+        std::cout << "]" << std::endl;
+    }
 
 private:
     std::list<T*> _items;
-    C comp;
 };
 
 #endif // ORDERED_LIST_H
