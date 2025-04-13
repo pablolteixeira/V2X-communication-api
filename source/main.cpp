4593e7078db3e844e10f78e36448f3efc9541f40
pablolteixeira <xpspablo@hotmail.com>
2025-04-13 14:57:19 -0300

    feat: create vehicle instances

--- source/main.cpp
+++ source/main.cpp
@@ -6,37 +6,22 @@
 #include "../header/raw_socket_engine.h"
 #include "../header/vehicle.h"
 
-struct TestMessage {
-    std::string text;
-};
-
 int main() {
     EthernetProtocol* protocol = EthernetProtocol::get_instance();
 
-    // lista de carros - mensagens iguais (de carro id_x - chegou em carro id_y - mensagem)
     EthernetNIC* nic1 = new EthernetNIC("NIC1");
-    EthernetNIC* nic2 = new EthernetNIC("NIC2");
+    //EthernetNIC* nic2 = new EthernetNIC("NIC2");
 
-    protocol->register_nic(nic1);
-    protocol->register_nic(nic2);
+    Vehicle* v = new Vehicle(1, nic1, protocol);
+    //Vehicle* v2 = new Vehicle(2, nic2, protocol);
 
-    EthernetProtocol::Address addr1(nic1->address(), 0);
-    EthernetProtocol::Address addr2(nic2->address(), 0);
+    v->start();
 
-    EthernetCommunicator* communicator1 = new EthernetCommunicator(protocol, addr1);
-    EthernetCommunicator* communicator2 = new EthernetCommunicator(protocol, addr2);
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 
-    Message* msg = new Message();
-    TestMessage* data = msg->get_data<TestMessage>();
-    data->text = "Mensagem de teste";
-    msg->size(sizeof(TestMessage));
-    communicator1->send(msg);
-
-    //Vehicle v1 = Vehicle(1, nic1);
-    //Vehicle v2 = Vehicle(2, nic2);
-
-    //v1.start();
-    //v2.start();
+    v->stop();
 
     return 0;
 }
