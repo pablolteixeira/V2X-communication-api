3efc0221904c20ebc11d1b91f54d88afc249b541
pablolteixeira <xpspablo@hotmail.com>
2025-04-18 16:32:54 -0300

    feat: add processes creation in main file

--- source/main.cpp
+++ source/main.cpp
@@ -6,22 +6,64 @@
 #include "../header/raw_socket_engine.h"
 #include "../header/vehicle.h"
 
+#include <sys/wait.h>
+
+const unsigned int NUM_VEHICLE = 2;
+
 int main() {
+    pid_t children_pids[NUM_VEHICLE];
+
     EthernetProtocol* protocol = EthernetProtocol::get_instance();
 
-    EthernetNIC* nic1 = new EthernetNIC("NIC1");
-    //EthernetNIC* nic2 = new EthernetNIC("NIC2");
+    //std::cout << "Father process: " << getpid() << std::endl;
 
-    Vehicle* v = new Vehicle(1, nic1, protocol);
-    //Vehicle* v2 = new Vehicle(2, nic2, protocol);
+    EthernetNIC *nic1 = new EthernetNIC("1");
+    EthernetNIC *nic2 = new EthernetNIC("2");
 
-    v->start();
+    Vehicle* vehicle1 = new Vehicle(nic1, protocol);
+    Vehicle* vehicle2 = new Vehicle(nic2, protocol);
+    
+    vehicle1->start();
+    vehicle2->start();
+    
+    // for (int i = 0; i < NUM_VEHICLE; i++) {
+    //     pid_t pid = fork();
+        
+    //     if (pid < 0) {
+    //         // Fork failed
+    //         std::cout << "Fork failed for process: " << i << std::endl;
+    //         continue;
+    //     } else if (pid == 0) {
+    //         std::cout << "Children process: " << getpid() << std::endl;
 
-    while (true) {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
+    //         std::string id = "NIC" + std::to_string(getpid());
+    //         EthernetNIC *nic = new EthernetNIC(id);
 
-    v->stop();
+    //         Vehicle* vehicle = new Vehicle(nic, protocol);
+
+    //         vehicle->start();
+    //         std::cout << "Vehicle " << id << " created" << std::endl;
+            
+    //         delete vehicle;
+    //         delete nic;
+    //         exit(0);
+    //     } else {
+    //         // Parent process
+    //         std::cout << "Created child process " << i << " with PID: " << pid << std::endl;
+    //         children_pids[i] = pid;
+    //     }
+    // }
+
+    
+    
+    // for (pid_t child_pid : children_pids) {
+    //     int status;
+    //     waitpid(child_pid, &status, 0);
+    //     std::cout << "Child process (PID: " << child_pid << ") has finished with status: " 
+    //               << WEXITSTATUS(status) << std::endl;
+    // }
+
+    // std::cout << "Parent process (PID: " << getpid() << ") finished." << std::endl;
 
     return 0;
 }
