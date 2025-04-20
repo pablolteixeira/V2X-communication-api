2cebc2391ee53fc75f323cdf4e4209c2cdcb5937
pablolteixeira <xpspablo@hotmail.com>
2025-04-19 23:48:57 -0300

    feat: create processes and use mmap to share the protocol instance

--- source/main.cpp
+++ source/main.cpp
@@ -8,62 +8,57 @@
 
 #include <sys/wait.h>
 
-const unsigned int NUM_VEHICLE = 2;
+const unsigned int NUM_VEHICLE = 10;
 
 int main() {
+    std::cout.setf(std::ios_base::unitbuf);
+
     pid_t children_pids[NUM_VEHICLE];
 
     EthernetProtocol* protocol = EthernetProtocol::get_instance();
-
-    //std::cout << "Father process: " << getpid() << std::endl;
-
-    EthernetNIC *nic1 = new EthernetNIC("1");
-    EthernetNIC *nic2 = new EthernetNIC("2");
-
-    Vehicle* vehicle1 = new Vehicle(nic1, protocol);
-    Vehicle* vehicle2 = new Vehicle(nic2, protocol);
     
-    vehicle1->start();
-    vehicle2->start();
-    
-    // for (int i = 0; i < NUM_VEHICLE; i++) {
-    //     pid_t pid = fork();
+    for (int i = 0; i < NUM_VEHICLE; i++) {
+        pid_t pid = fork();
         
-    //     if (pid < 0) {
-    //         // Fork failed
-    //         std::cout << "Fork failed for process: " << i << std::endl;
-    //         continue;
-    //     } else if (pid == 0) {
-    //         std::cout << "Children process: " << getpid() << std::endl;
+        if (pid < 0) {
+            // Fork failed
+            std::cout << "Fork failed for process: " << i << std::endl;
+            continue;
+        } else if (pid == 0) {
+            std::cout << "Children process: " << getpid() << std::endl;
 
-    //         std::string id = "NIC" + std::to_string(getpid());
-    //         EthernetNIC *nic = new EthernetNIC(id);
+            std::string id = "NIC" + std::to_string(getpid());
+            EthernetNIC *nic = new EthernetNIC(id);
 
-    //         Vehicle* vehicle = new Vehicle(nic, protocol);
+            EthernetProtocol* child_protocol = EthernetProtocol::get_instance();
 
-    //         vehicle->start();
-    //         std::cout << "Vehicle " << id << " created" << std::endl;
+            Vehicle* vehicle = new Vehicle(nic, child_protocol);
+
+            vehicle->start();
+            std::cout << "Vehicle " << id << " created" << std::endl;
             
-    //         delete vehicle;
-    //         delete nic;
-    //         exit(0);
-    //     } else {
-    //         // Parent process
-    //         std::cout << "Created child process " << i << " with PID: " << pid << std::endl;
-    //         children_pids[i] = pid;
-    //     }
-    // }
+            vehicle->stop();
 
+            delete vehicle;
+            delete nic;
+            exit(0);
+        } else {
+            // Parent process
+            std::cout << "Created child process " << i << " with PID: " << pid << std::endl;
+            children_pids[i] = pid;
+        }
+    }
     
-    
-    // for (pid_t child_pid : children_pids) {
-    //     int status;
-    //     waitpid(child_pid, &status, 0);
-    //     std::cout << "Child process (PID: " << child_pid << ") has finished with status: " 
-    //               << WEXITSTATUS(status) << std::endl;
-    // }
+    for (pid_t child_pid : children_pids) {
+        int status;
+        waitpid(child_pid, &status, 0);
+        std::cout << "Child process (PID: " << child_pid << ") has finished with status: " 
+                  << WEXITSTATUS(status) << std::endl;
+    }
 
-    // std::cout << "Parent process (PID: " << getpid() << ") finished." << std::endl;
+    //delete protocol;
+    
+    std::cout << "Parent process (PID: " << getpid() << ") finished." << std::endl;
 
     return 0;
 }
