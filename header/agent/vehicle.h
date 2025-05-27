75917122814fd58db519ade11b1f90d61f0843d4
Tiago-O-Luz <tiagoluz64@gmail.com>
2025-05-27 20:47:04 -0300

    ADD: Vehicle class reestructured

--- header/agent/vehicle.h
+++ header/agent/vehicle.h
@@ -1,14 +1,24 @@
 #ifndef VEHICLE_H
 #define VEHICLE_H
 
+#include <vector>
+#include "../traits.h"
 #include "../autonomous_agent.h"
+#include "../component.h"
 
-class SmartData;
+// class Component;
 
 class Vehicle: public AutonomousAgent
 {
 public:
     Vehicle(EthernetNIC* nic, EthernetProtocol* protocol);
+    ~Vehicle();
+    
+    void start() override;
+    void stop() override;
+
+protected:
+    std::vector<Component*> _components;
 };
 
 #endif // VEHICLE_H
--- source/vehicle.cpp
+++ source/vehicle.cpp
@@ -1,4 +1,3 @@
-#include "../header/nic.h"
 #include "../header/smart_data.h"
 #include "../header/agent/vehicle.h"
 #include "../header/component/lidar_component.h"
@@ -15,4 +14,37 @@ Vehicle::Vehicle(EthernetNIC* nic, EthernetProtocol* protocol)
     _components.push_back(new GPSComponent(this, 2));
     _components.push_back(new ControllerComponent(this, 3));
     _components.push_back(new SteeringComponent(this, 4));
+}
+
+Vehicle::~Vehicle() {
+    for(Component* component: _components) {
+        delete component;
+    }
+}
+
+void Vehicle::start() {
+    ConsoleLogger::log("Starting Vehicle -> " + std::to_string(_id));
+    
+    if (_running) {
+        ConsoleLogger::log("Running: " + std::to_string(_running));
+    }
+    ConsoleLogger::log("Starting Components");
+    for(Component* component : _components) {
+        component->start();
+    }
+    ConsoleLogger::log("Components started");    
+
+    _running = true;
+    ConsoleLogger::log("Threads running.");
+}
+
+void Vehicle::stop() {
+    ConsoleLogger::log("Stopping Vehicle -> " + std::to_string(_id));
+    _running = false;
+    
+    for(Component* component: _components) {
+        component->stop();
+    }
+
+    _nic->stop();
 }
