43901089f1745a961ac5ec7691c47a6ffbb9eb1a
Tiago-O-Luz <tiagoluz64@gmail.com>
2025-05-27 21:14:43 -0300

    ADD: added time update logic, fake offset for randomness

--- header/time_keeper.h
+++ header/time_keeper.h
@@ -2,26 +2,46 @@
 #define TIME_KEEPER_H
 
 #include <chrono>
+#include <random>
+
 #include "u64_type.h"
+#include "ethernet.h"
+
+#define SYNC_TIMEOUT 50;
+
 
 class TimeKeeper 
 {
 public:
+    typedef Ethernet::Footer::SyncState SyncState;
+    typedef Ethernet::Footer::PacketOrigin PacketOrigin;
+
     TimeKeeper();
     ~TimeKeeper();
 
     U64 get_system_timestamp();
     U64 get_local_timestamp();
+    SyncState get_sync_state();
+    PacketOrigin get_packet_origin();
+
+    void update_packet_origin(PacketOrigin packet_origin);
+    void update_sync_state(SyncState sync_sate);
     void update_time_keeper(U64 system_timestamp, U64 local_timestamp);
-private:
 
 private:
-    U64 offset = 0;
     bool is_t1 = true;
+    int fake_offset;
+    
+    U64 offset = 0;
+    U64 last_update;
     U64 t1 = 0;
     U64 t2 = 0;
     U64 t3 = 0;
     U64 t4 = 0;
+    
+    PacketOrigin _packet_origin = PacketOrigin::OTHERS;
+    SyncState _sync_state = SyncState::NOT_SYNCHRONIZED;
+    U64 sync_timeout = SYNC_TIMEOUT;
 };
 
 #endif // TIME_KEEPER_H 
--- source/time_keeper.cpp
+++ source/time_keeper.cpp
@@ -1,29 +1,58 @@
 #include "../header/time_keeper.h"
 
-TimeKeeper::TimeKeeper() {}
+TimeKeeper::TimeKeeper() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::uniform_int_distribution<int> dist(-100, 100);
+    fake_offset = dist(gen);
+    sync_timeout *= 1000;
+}
 
 U64 TimeKeeper::get_system_timestamp() {
     return get_local_timestamp() - offset;
 }
 
+TimeKeeper::SyncState TimeKeeper::get_sync_state() {
+    return _sync_state;
+}
+
+void TimeKeeper::update_sync_state(SyncState new_state) {
+    _sync_state = new_state;
+}
+
+TimeKeeper::PacketOrigin TimeKeeper::get_packet_origin() {
+    return  _packet_origin;
+}
+
+void TimeKeeper::update_packet_origin(PacketOrigin packet_origin) {
+    _packet_origin = packet_origin;
+}
+
 U64 TimeKeeper::get_local_timestamp() {
     auto now = std::chrono::system_clock::now();
     auto duration = now.time_since_epoch();
-    auto timestamp = std::chrono::duration_cast<std::chrono::microseconds>(duration);
+    auto timestamp = std::chrono::duration_cast<std::chrono::microseconds>(duration) + 
+                     std::chrono::microseconds(fake_offset) +
+                     std::chrono::microseconds(offset);
 
     return timestamp.count();
 }
 
 void TimeKeeper::update_time_keeper(U64 system_timestamp, U64 local_timestamp) {
-    if (is_t1) {
-        t1 = system_timestamp;
-        t2 = local_timestamp;
-    } else {
-        t3 = system_timestamp;
-        t4 = local_timestamp;
-
-        U64 delay = ((t4 - t3) + (t2  - t1)) / 2;
-        offset = (t2 - t1) - delay;
+    auto elapsed_time = local_timestamp - last_update;
+    if (elapsed_time > sync_timeout * 10/100) {    
+        if (is_t1) {
+            t1 = system_timestamp;
+            t2 = local_timestamp;
+        } else {
+            t3 = system_timestamp;
+            t4 = local_timestamp;
+            
+            U64 delay = ((t4 - t3) + (t2  - t1)) / 2;
+            offset = (t2 - t1) - delay;
+            last_update = local_timestamp + offset;
+        }
+        is_t1 = !is_t1;
     }
-    is_t1 = !is_t1;
 }
