5ebe8993c0e4335b5cd5900bf4b41421d7fb95ab
Tiago-O-Luz <tiagoluz64@gmail.com>
2025-06-02 13:03:52 -0300

    ADD: mac field in metadata

--- header/ethernet.h
+++ header/ethernet.h
@@ -24,6 +24,7 @@ public:
     typedef struct ethhdr Header;
     typedef unsigned char Address[ETH_ALEN];
     typedef unsigned short Protocol;
+    typedef uint32_t MAC;
 
     class Metadata 
     {
@@ -41,10 +42,12 @@ public:
         Metadata() : _timestamp(0), _sync_state(SyncState::NOT_SYNCHRONIZED), _packet_origin(PacketOrigin::OTHERS) {}
         Metadata(U64 timestamp, SyncState sync_state, PacketOrigin packet_origin) : _timestamp(timestamp), _sync_state(sync_state), _packet_origin(packet_origin) {}
 
+        MAC get_mac() {return _mac; }
         U64 get_timestamp() { return _timestamp; }
         SyncState get_sync_state() { return _sync_state; }
         PacketOrigin get_packet_origin() {return _packet_origin; }
 
+        void set_mac(MAC mac) {_mac = mac; }
         void set_timestamp(U64 timestamp) {_timestamp = timestamp; }
         void set_sync_state(SyncState sync_state) { _sync_state = sync_state; }
         void set_packet_origin(PacketOrigin packet_origin) { _packet_origin = packet_origin; }
@@ -52,6 +55,7 @@ public:
     private:
         U64 _timestamp;
         SyncState _sync_state;
+        MAC _mac;
         PacketOrigin _packet_origin;
     } __attribute__((packed));
 
